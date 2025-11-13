e8e93b133da390a105d43d58df59f6f4cdcb427c
Percy <percy.smith@colorado.edu>
2025-11-13 15:37:49 -0700

    4.0.0 Correction

--- V4.0.0/LiveVis_RETIGO_V4_0_0.m
+++ V4.0.0/LiveVis_RETIGO_V4_0_0.m
@@ -5,17 +5,15 @@
 % @author   Percy Smith, percy.smith@colorado.edu   
 % @brief    Firmware meant to visualize POD data live! 
 %
-% @date     January 23, 2025
-% @version  3.5.1
-% @log      Made firmware backwards compatible with MATLAB pre-2023b 
-%           Error reported 01/21/25 by Kyliana Gailitis (thank you!)
-%           - colororder does not exist prior to 2023b
+% @date     November 13, 2025
+% @version  4.0.0
+% @log      Second update of serial monitor reading & data
 %
 %% Terminate
 close all; clear; 
 
 %% SETTINGS
-COMPORT = "/dev/cu.usbmodem11401";
+COMPORT = "COM5";
     %if u don't know the com port, run serialportlist in command window
 
 %% Initialize variables and establish serial connection
@@ -28,12 +26,13 @@ device.Timeout = 60;
 
 %% Callback
 function myCallback(src, ~)
-    plottype = 4;
+    plottype = 6;
         % [1] CO_2 only (10)
         % [2] VOC only (x2 channel) (6 7)
         % [3] PM 2.5 only  (12)
         % [4] SHOW ALL!!!!
         % [5] Car Exhaust Visualization +CO (9), VOC(6 7), 
+        % [6] Show All - CO & PM
     data = readline(src);
     src.UserData = [src.UserData; strsplit(data, ',')];
     switch plottype
@@ -47,6 +46,8 @@ function myCallback(src, ~)
             SHOW_ALL(src);
         case 5
             CAR_EMISSIONS(src);
+        case 6
+            CO_PM_ALL(src);
         otherwise
             alternateGraph(src);
     end
@@ -147,9 +148,9 @@ function SHOW_ALL(src)
     M = [2 88 14; 58 200 49; 255 214 10; 254 144 67; 192 76 11; 250 138 212; 125 169 255];
         M = M./255;
     t = datetime(src.UserData(:,1)) + hours(1);
-    heavyVOC = str2double(src.UserData(:,4));
-    lightVOC = str2double(src.UserData(:,5));
-    CO_2 = str2double(src.UserData(:,8));
+    heavyVOC = str2double(src.UserData(:,5));
+    lightVOC = str2double(src.UserData(:,6));
+    CO_2 = str2double(src.UserData(:,9));
     PM25 = str2double(src.UserData(:,10));
     T = str2double(src.UserData(:,2));
     RH = str2double(src.UserData(:,3));
@@ -228,10 +229,10 @@ function CAR_EMISSIONS(src)
     M = [2 88 14; 58 200 49; 255 214 10; 254 144 67; 192 76 11; 250 138 212; 125 169 255];
         M = M./255;
     t = datetime(src.UserData(:,1)) + hours(1);
-    heavyVOC = str2double(src.UserData(:,4));
-    lightVOC = str2double(src.UserData(:,5));
-    CO_2 = str2double(src.UserData(:,8));
-    CO = str2double(src.UserData(:,7));
+    heavyVOC = str2double(src.UserData(:,5));
+    lightVOC = str2double(src.UserData(:,6));
+    CO_2 = str2double(src.UserData(:,9));
+    CO = str2double(src.UserData(:,8));
     T = str2double(src.UserData(:,2));
     RH = str2double(src.UserData(:,3));
     tbl = timetable(t, heavyVOC, lightVOC, CO_2, CO, T, RH);
@@ -277,7 +278,7 @@ function CAR_EMISSIONS(src)
         plot(tbl.t, tbl.CO, 'Color', M(7,:), 'LineWidth', 1.5);
             grid on;
         title('Carbon Monoxide Signal (CO)');
-            ylabel('Signal [ADU]');
+            ylabel('CO [ppb]');
                 % ylim([0 max(CO)+10]);
                 xlim([min(t)-seconds(5), max(t)+seconds(5)]);
             % legend('PM 2.5 [ug/m^3]', 'Location', 'northwest');
@@ -299,6 +300,92 @@ function CAR_EMISSIONS(src)
             % legend('Heavy VOC [ADU]', 'Location', 'northwest');
     refreshdata;
 end
+
+%% CASE 6: CO & PM SHOW_ALL
+function CO_PM_ALL(src)
+    % Used to be orderedcolors('reef') - corrected 1/23/25
+    R = [221 84 0; 84 182 255; 17 113 190; 254 114 67; 116 235 218; 0 163 163];
+        R = R./255;
+    % Used to be orderedcolors('meadow') - corrected 1/23/25
+    M = [2 88 14; 58 200 49; 255 214 10; 254 144 67; 192 76 11; 250 138 212; 125 169 255];
+        M = M./255;
+    t = datetime(src.UserData(:,1)) + hours(1);
+    heavyVOC = str2double(src.UserData(:,5));
+    lightVOC = str2double(src.UserData(:,6));
+    CO = str2double(src.UserData(:,8));
+    CO_2 = str2double(src.UserData(:,9));
+    PM25 = str2double(src.UserData(:,10));
+    T = str2double(src.UserData(:,2));
+    RH = str2double(src.UserData(:,3));
+    tbl = timetable(t, heavyVOC, lightVOC, CO, CO_2, PM25, T, RH);
+
+    sz = 25;
+
+    tiledlayout(5,2, 'TileSpacing', 'tight', 'Padding', 'tight');
+    ax1 = nexttile(1);
+        % TEMP_LIMS = [0.99*min(tbl.T) 1.01*min(tbl.T)]; 
+        plot(tbl.t, tbl.T, 'k');
+        hold on;
+        scatter(tbl.t, tbl.T, sz, tbl.T,'filled');
+        grid on;
+        title('Temperature');
+        ylabel('Temperature [\circC]');
+        xlim([min(t)-seconds(5), max(t)+seconds(5)]);
+        % ylim(TEMP_LIMS);
+        colorbar(ax1);
+        % clim(TEMP_LIMS);
+        colormap(ax1, "hot");
+    ax2 = nexttile(2);
+        RH_LIMS = [0.99*min(tbl.RH) 1.01*max(tbl.RH)];
+        plot(tbl.t, tbl.RH, 'k');
+        hold on;
+        scatter(tbl.t, tbl.RH, 20, tbl.RH,'filled');
+        grid on;
+        title('Relative Humidity');
+        ylabel('Humidity [% RH]');
+        xlim([min(t)-seconds(5), max(t)+seconds(5)]);
+        ylim(RH_LIMS);
+        colorbar(ax2);
+        clim(RH_LIMS);
+        colormap(ax2, sky);
+    nexttile(4, [2 1]);
+        plot(tbl.t, tbl.CO_2, 'Color', R(4,:), 'LineWidth', 1.5);
+            grid on;
+        title('Carbon Dioxide (CO_2)');
+            ylabel('CO_2 [ppm]');
+                ylim([min(tbl.CO_2)*0.99 max(tbl.CO_2)*1.01]);
+                xlim([min(t)-seconds(5), max(t)+seconds(5)]);
+            % legend('CO_2 [ppm]', 'Location', 'northwest');
+    nexttile(8, [2 1]);
+        plot(tbl.t, tbl.PM25, 'Color', R(5,:), 'LineWidth', 1.5);
+            grid on;
+        title('Particulate Matter 2.5');
+            ylabel('PM 2.5 [ug/m^3]');
+                ylim([0 max(PM25)+10]);
+                xlim([min(t)-seconds(5), max(t)+seconds(5)]);
+            % legend('PM 2.5 [ug/m^3]', 'Location', 'northwest');
+    nexttile(3, [2 1]);
+        plot(tbl.t, tbl.lightVOC, 'Color', M(2,:), 'LineWidth', 1.5);
+            grid on;
+            hold on;
+        plot(tbl.t, tbl.heavyVOC, 'Color', M(1,:), 'LineWidth', 1.5);
+        title('VOC Signal');
+            ylabel('Signal [ADU]');
+                ylim([min(min(tbl.lightVOC), min(tbl.heavyVOC))*0.99 max(max(tbl.lightVOC), max(tbl.heavyVOC))*1.01]);
+                xlim([min(t)-seconds(5), max(t)+seconds(5)]);
+            % legend('Light VOC [ADU]', 'Location', 'northwest');
+    nexttile(7, [2 1]);
+        plot(tbl.t, tbl.CO, 'Color', M(7,:), 'LineWidth', 1.5);
+            grid on;
+        title('Carbon Monoxide Signal (CO)');
+            ylabel('CO [ppb]');
+                % ylim([0 max(CO)+10]);
+                xlim([min(t)-seconds(5), max(t)+seconds(5)]);
+            % legend('PM 2.5 [ug/m^3]', 'Location', 'northwest');
+    refreshdata;
+end
+
+
 %% REF: https://www.mathworks.com/help/matlab/ref/stackedplot.html
 function alternateGraph(src)
     t = datetime(src.UserData(:, 1)) + hours(1);
@@ -330,8 +417,6 @@ end
 %     'Fig2600(bits)'; 'Fig2602(bits)'; 'Ozone(bits)'; 'CO(bits)'; 'CO_2(ppm)';...
 %     'PM1.0(ug/m^3)'; 'PM2.5(ug/m^3)'; 'PM10.0(ug/m^3)'];
 % Display_Header = ['DateTime';
-%     'Temperature_{BME}';
-%     'Pressure_{BME}';
 %     'Temperature';
 %     'Relative Humidity';
 %     'Light VOC';
